94a586c31b26433a93a84d7fb4c969bc2da244d1
mehedi494 <ehasanmunna21@gmail.com>
2022-03-24 12:30:19 +0600

    Complete

--- app.js
+++ app.js
@@ -1,25 +1,72 @@
+//........................ GENERATE PIN SECTION START.............................
+// ..............................................................................................
+
 document.getElementById("generateBtn").addEventListener("click", function(){
-    // console.log("worked")
-    generateNumberFc()
+   
+    getPin()
+    
 })
+
+
 function generateNumberFc() {
     let generateNumber = Math.ceil(Math.random() * 10000)
-    let generateNumberSting = generateNumber+""
+    let generateNumberSting = generateNumber + '';
     if (generateNumberSting.length == 4) {
-      const newGenerateNumber = parseInt(generateNumberSting)
-        console.log(newGenerateNumber)
-        const inputString = document.getElementById("generateInputField")
-         return inputString.value = newGenerateNumber;
+      
+         return generateNumber
     }
     else {
    return generateNumberFc()
     }
-
-    
-
 }
-function pinMatch() {
-    
-    
-
+function getPin() {
+    const newPin = generateNumberFc()
+    document.getElementById("generateInputField").value = newPin
 }
+// ..................................................................................................
+
+
+//.............................. Keyapad START ........................................................
+// ....................................................................................................
+
+
+document.getElementById("bubble").addEventListener("click", function (event) {
+    const number = event.target.innerText;
+    const display = document.getElementById("displayInput");
+    if (!isNaN(number)) {
+        const oldNumber = display.value 
+        const newNumber = oldNumber + number;
+        display.value = newNumber
+        
+    }
+    else  {
+        if (number == "C") {
+            display.value = '';
+        }
+        else if (number == "<") {
+            console.log("delet")
+        }
+        
+    }
+})
+document.getElementById("submitBtn").addEventListener("click", function () {
+    
+})
+ 
+// ..............................SUBMISSION START...................
+// .......................................................................................
+document.getElementById("submitBtn").addEventListener("click",function () {
+    const display = document.getElementById("displayInput").value
+    const pinDisplay = document.getElementById("generateInputField").value
+
+    if (pinDisplay == display && pinDisplay != '') {
+        document.getElementById("success").style.display="block"
+        document.getElementById("failed").style.display = "none"
+        console.log("yes")
+    }
+    else {
+        console.log("NO")
+        document.getElementById("success").style.display = "none"
+        document.getElementById("failed").style.display = "block"
+    }
+})
